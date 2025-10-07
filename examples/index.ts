01c75fdde5ae7452389669b59166acd3ef7fe9e7
Ethan Niser <ethan@niser.com>
2025-10-06 23:44:42 -0700

    update example

--- examples/index.ts
+++ examples/index.ts
@@ -1,10 +1,22 @@
 import { S2 } from "../src";
 
 const s2 = new S2({
-  accessToken: "123",
+  accessToken: process.env.S2_ACCESS_TOKEN!,
 });
 
 const basins = await s2.basins.list();
-const accessTokens = await s2.accessTokens.list();
-const basin = s2.basin("test");
+console.log(
+  "basins",
+  basins.basins.map((basin) => basin.name)
+);
+if (!basins.basins[0]) {
+  throw new Error("No basin found");
+}
+const basin = s2.basin(basins.basins[0].name);
 const streams = await basin.streams.list();
+const streamResults = await Promise.all(
+  streams.streams.map((stream) =>
+    basin.streams.getConfig({ stream: stream.name })
+  )
+);
+console.log("streams", streamResults);
