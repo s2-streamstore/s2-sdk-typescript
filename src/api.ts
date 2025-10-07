a22c659dc0ce43a38927b5b9e46babf015497e81
Ethan Niser <ethan@niser.com>
2025-10-06 21:49:52 -0700

    heyapi

--- src/api.ts
+++ src/api.ts
@@ -1,33 +1,3 @@
-import createClient from "openapi-fetch";
-import type { paths } from "./generated/api";
+import { AppendResponse } from "./generated2/types.gen";
 
-type BasinSpecificApiPaths =
-  | "/streams/{stream}"
-  | "/streams/{stream}/records"
-  | "/streams/{stream}/records/tail";
-
-// Get only the basin-specific paths
-type basinSpecificPaths = Pick<paths, BasinSpecificApiPaths>;
-
-// Get all paths EXCEPT the basin-specific ones
-type generalPaths = Omit<paths, BasinSpecificApiPaths>;
-
-const generalApiClient = createClient<generalPaths>({
-  baseUrl: "https://api.s2.streamstore.com",
-});
-
-const makeBasinSpecificApiClient = (basin: string) => {
-  return createClient<basinSpecificPaths>({
-    baseUrl: `https://${basin}.b.aws.s2.dev`,
-  });
-};
-
-generalApiClient.GET("/streams");
-const basinSpecificClient = makeBasinSpecificApiClient("test");
-basinSpecificClient.GET("/streams/{stream}", {
-  params: {
-    path: {
-      stream: "test",
-    },
-  },
-});
+type Output = AppendResponse;
