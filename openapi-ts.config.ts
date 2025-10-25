ba800ccb2805980f2b4a5e8370a53fce0df3946b
Ethan Niser <ethan@niser.com>
2025-10-25 09:55:19 -0700

    fix openapi gen

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "@hey-api/openapi-ts";
 export default defineConfig({
   input:
     "https://raw.githubusercontent.com/s2-streamstore/s2-protos/refs/heads/main/s2/v1/openapi.json", // sign up at app.heyapi.dev
-  output: "src/generated",
+  output: {
+    path: "src/generated",
+    importFileExtension: ".js",
+  },
 });
