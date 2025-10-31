0da5958ebac739f6acc7e7aa458cf1f698e4f0c2
Mehul Arora <mehul@s2.dev>
2025-10-31 14:40:56 -0400

    fix: no additonal headers for cmd records

--- src/utils.ts
+++ src/utils.ts
@@ -20,61 +20,39 @@ export const AppendRecord = {
 		body?: string | Uint8Array,
 		headers?: Headers,
 		timestamp?: AppendRecordType["timestamp"],
-	): AppendRecordType => {
-		return {
-			body,
-			headers,
-			timestamp,
-		};
-	},
+	): AppendRecordType => ({
+		body,
+		headers,
+		timestamp,
+	}),
 	command: (
 		/** Command name (e.g. "fence" or "trim"). */
 		command: string,
 		body?: string | Uint8Array,
-		additionalHeaders?: Headers,
 		timestamp?: AppendRecordType["timestamp"],
-	): AppendRecordType => {
-		const headers: AppendRecordType["headers"] = (() => {
-			if (!additionalHeaders) {
-				return [["", command]];
-			} else if (Array.isArray(additionalHeaders)) {
-				return [["", command] as [string, string], ...additionalHeaders];
-			} else {
-				return [
-					["", command] as [string, string],
-					...Object.entries(additionalHeaders).map(
-						([key, value]) => [key, value] as [string, string],
-					),
-				];
-			}
-		})();
-		return {
-			body,
-			headers,
-			timestamp,
-		};
-	},
+	): AppendRecordType => ({
+		body,
+		headers: [["", command]],
+		timestamp,
+	}),
 	fence: (
 		fencing_token: string,
-		additionalHeaders?: Headers,
 		timestamp?: AppendRecordType["timestamp"],
 	) => {
 		return AppendRecord.command(
 			"fence",
 			fencing_token,
-			additionalHeaders,
 			timestamp,
 		);
 	},
 	trim: (
-		seqNum: number | bigint,
-		additionalHeaders?: Headers,
+		seqNum: number | bigint,		
 		timestamp?: AppendRecordType["timestamp"],
 	): AppendRecordType => {
 		// Encode sequence number as 8 big-endian bytes
 		const buffer = new Uint8Array(8);
 		const view = new DataView(buffer.buffer);
 		view.setBigUint64(0, BigInt(seqNum), false); // false = big-endian
-		return AppendRecord.command("trim", buffer, additionalHeaders, timestamp);
+		return AppendRecord.command("trim", buffer, timestamp);
 	},
 } as const;
