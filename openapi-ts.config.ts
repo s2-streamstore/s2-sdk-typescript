8ae8492c8b21f5daf804ebb080233c813e2e3cbe
Mehul Arora <mehul@s2.dev>
2026-01-15 23:01:12 -0500

    chore: update specs + run tests with s2-lite (#106)

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  input: "./s2-protos/s2/v1/openapi.json",
+  input: "./s2-specs/s2/v1/openapi.json",
   output: {
     path: "packages/streamstore/src/generated",
     importFileExtension: ".js",
